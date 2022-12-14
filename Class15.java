b7cf90ac2d98982ca04d5bceb1d2b75f753bbe72
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-14 14:51:02 +0800

    Create Class15.java

---
+++ Class15.java
@@ -0,0 +1,10 @@
+public class hw12_15
+ {
+ public static void main(String args[])
+ {
+ String str;
+ str=Float.toString(123.45f);
+ System.out.println(str);
+ System.out.println("length="+str.length());
+ }
+ }
