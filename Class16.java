228e286a496d88d9a6f8d4d38b464ccbeef93e13
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-14 14:51:56 +0800

    Create Class16.java

---
+++ Class16.java
@@ -0,0 +1,11 @@
+public class hw12_16
+ {
+ public static void main(String args[])
+ {
+ String str="262904713";
+
+ int num;
+ num=Integer.parseInt(str);
+ System.out.println(num+"*2="+num*2);
+ }
+ }
