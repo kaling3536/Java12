33b63da8076c10b51b84ce3cb19119161676760d
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-14 14:50:21 +0800

    Create Class14.java

---
+++ Class14.java
@@ -0,0 +1,10 @@
+public class hw12_14
+ {
+ public static void main(String args[])
+ {
+
+ String str="Habit is second nature.";
+ System.out.println("string="+str);
+ System.out.println("sub-string="+str.substring(16,22));
+ }
+ }
