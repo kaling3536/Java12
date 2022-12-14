dbbee13e2ce22db5b1bc509c792dd9a69c402713
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-14 14:52:58 +0800

    Create Class17.java

---
+++ Class17.java
@@ -0,0 +1,10 @@
+public class hw12_17
+ {
+ public static void main(String args[])
+ {
+ String str="262904713";
+ short num;
+ num=Short.parseShort(str);
+ System.out.println(num+"*2="+num*2);
+ }
+ }
